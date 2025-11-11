52966a980c45f30c48f118fa5cc46c244c010e4d
oksankah <oksana.harvasiuk@gmail.com>
2025-11-11 17:00:33 +0200

    .

--- controllers/productRoutes.js
+++ controllers/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"; 
-import { getProducts, createProduct } from "controllers/productController.js"; 
+import { getProducts, createProduct, updateProduct, deleteProduct } from "./productController.js"; 
 import { validateProduct } from "../middleware/validateProduct.js"; 
 const router = express.Router(); 
 router.get("/", getProducts); 
