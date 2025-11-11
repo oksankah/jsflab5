41f1270229cfab7017e3a810b910257bc39110e8
oksankah <oksana.harvasiuk@gmail.com>
2025-11-11 20:00:24 +0200

    .

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express"; 
 import cors from "cors"; 
 import dotenv from "dotenv"; 
-import { connectDB } from "./services/db.js"; 
+import { connectDB } from "../services/db.js"; 
 import productRoutes from "./controllers/productRoutes.js"; [cite_start]
 dotenv.config(); 
 const app = express(); 
