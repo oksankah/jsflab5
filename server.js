4e852155bf1f807e9d074fe9bb8b810a1d176746
oksankah <oksana.harvasiuk@gmail.com>
2025-11-18 23:50:42 +0200

    .

--- server.js
+++ server.js
@@ -3,31 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv"; 
 import { connectDB } from "./services/db.js"; 
 import productRoutes from "./src/controllers/productRoutes.js"; 
+
 dotenv.config(); 
 const app = express(); 
-// CORS setup (allow only your UI domain) 
-const allowedOrigins = process.env.CLIENT_ORIGINS.split(","); 
-app.use( 
-cors({ 
-origin: (origin, callback) => { 
-// allow REST tools like Postman (no origin) 
-if (!origin) return callback(null, true); 
-if (allowedOrigins.includes(origin)) {
-return callback(null, true); 
-} 
-callback(new Error("Not allowed by CORS")); 
-}, 
-credentials: true, 
-methods: ["GET", "POST", "PUT", "DELETE"], 
-}) 
-); 
-// Middleware 
+
+app.use(cors({
+  origin: "*" 
+}));
+
 app.use(express.json()); 
-// Routes 
+
 app.use("/api/products", productRoutes); 
-// DB Connection 
+
 const dbURI = process.env.MONGO_URI; 
 connectDB(dbURI); 
-// Start server 
+
 const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
